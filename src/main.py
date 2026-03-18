4f4b1dbb04148201a6a211cea46c0e723ab28431
DarkoGjichev <darkogicev111@gmail.com>
2026-03-18 16:29:18 +0000

    chore: add envs, create compose file for mongo and setup lifespan in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from skills-tracker-backend!")
+from contextlib import asynccontextmanager
+from pymongo import AsyncMongoClient
+from fastapi import FastAPI
+from beanie import init_beanie
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        client = AsyncMongoClient("mongodb://admin:password@localhost:27019/")
+        await init_beanie(database=client["skills_tracker_db"])
+    except Exception:
+        raise
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
